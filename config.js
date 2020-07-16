9c01151bbad010b8ca10d504b5121714ef652f19
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-16 11:49:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  Token: '',
+  Token: 'NjUzMTczMTM0ODA3MTM4MzE2.XxAiUg.T5Wce7Sr5qStuXfaumuWrABSs_c',
   Prefix: '!',
   webport: 3000,
   staffRoles: [
-    '',
+    'lol',
     '',
   ],
 };
